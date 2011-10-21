d7168f95b65111a5c10223dfc6b4786e0943e3df
Alexandr Zaozerskiy <ZaozerskyAU@gmail.com>
2011-10-21 11:25:35 +0600

    Value placed on one line with tag.

--- indentxml.py
+++ indentxml.py
@@ -1,12 +1,14 @@
-import sublime, sublime_plugin, string
+import sublime, sublime_plugin, re
 from xml.dom.minidom import *
 
 class IndentxmlCommand(sublime_plugin.TextCommand):
     def run(self, edit):  
         view = self.view
-        for region in view.sel():  
-            if not region.empty():  
-                s = view.substr(region)  
+        for region in view.sel():
+            if not region.empty():
+                s = view.substr(region)
                 s = parseString(s).toprettyxml()
+                text_re = re.compile('>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL)
+                s = text_re.sub('>\g<1></', s)
                 s = s.replace("<?xml version=\"1.0\" ?>", "").strip()
                 view.replace(edit, region, s)
