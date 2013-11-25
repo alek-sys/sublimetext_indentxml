9157b7aad1a0cf0836cab4216ca07587d4dde9f3
Roman Zhuzha <romazhuzha@gmail.com>
2013-11-25 17:45:44 +0200

    first commit of my modified version

--- indentxml.py
+++ indentxml.py
@@ -1,9 +1,12 @@
 import sublime
 import sublime_plugin
 import re
-import json
+# import json
+import simplejson as json
 from xml.dom.minidom import *
 from os.path import basename
+import decimal
+from simplejson import OrderedDict
 
 
 class BaseIndentCommand(sublime_plugin.TextCommand):
@@ -11,7 +14,7 @@ class BaseIndentCommand(sublime_plugin.TextCommand):
         self.view = view
         self.language = self.get_language()
 
-    def get_language(self):        
+    def get_language(self):
         syntax = self.view.settings().get('syntax')
         language = basename(syntax).replace('.tmLanguage', '').lower() if syntax != None else "plain text"
         return language
@@ -38,21 +41,21 @@ class BaseIndentCommand(sublime_plugin.TextCommand):
         regions = view.sel()
         # if there are more than 1 region or region one and it's not empty
         if len(regions) > 1 or not regions[0].empty():
-                for region in view.sel():
-                    if not region.empty():
-                        s = view.substr(region).strip()
-                        s = self.indent(s)
-                        view.replace(edit, region, s)
+            for region in view.sel():
+                if not region.empty():
+                    s = view.substr(region).strip()
+                    s = self.indent(s)
+                    view.replace(edit, region, s)
         else:   #format all text
-                alltextreg = sublime.Region(0, view.size())
-                s = view.substr(alltextreg).strip()
-                s = self.indent(s)
-                view.replace(edit, alltextreg, s)
+            alltextreg = sublime.Region(0, view.size())
+            s = view.substr(alltextreg).strip()
+            s = self.indent(s)
+            view.replace(edit, alltextreg, s)
 
 
 class AutoIndentCommand(BaseIndentCommand):
     def get_text_type(self, s):
-        language =  self.language 
+        language =  self.language
         if language == 'xml':
             return 'xml'
         if language == 'json':
@@ -73,7 +76,7 @@ class AutoIndentCommand(BaseIndentCommand):
             command = IndentJsonCommand(self.view)
         if text_type == 'notsupported':
             return s
-        
+
         return command.indent(s)
 
     def check_enabled(self, lang):
@@ -81,27 +84,23 @@ class AutoIndentCommand(BaseIndentCommand):
 
 
 class IndentXmlCommand(BaseIndentCommand):
-    def indent(self, s):                
+    def indent(self, s):
         # convert to utf
-        s = s.encode("utf-8") 
-        xmlheader = re.compile(b"<\?.*\?>").match(s)
+        s = s.encode("utf-8")
+        xmlheader = re.compile(br"<\?.*\?>").match(s)
         # convert to plain string without indents and spaces
-        s = re.compile(b'>\s+([^\s])', re.DOTALL).sub(b'>\g<1>', s)
+        s = re.compile(br'>\s+([^\s])', re.DOTALL).sub(br'>\g<1>', s)
         # replace tags to convince minidom process cdata as text
-        s = s.replace(b'<![CDATA[', b'%CDATAESTART%').replace(b']]>', b'%CDATAEEND%') 
-        try:
-            s = parseString(s).toprettyxml()
-        except Exception as e:
-            sublime.active_window().run_command("show_panel", {"panel": "console", "toggle": True})
-            raise e
+        s = s.replace(b'<![CDATA[', b'%CDATAESTART%').replace(b']]>', b'%CDATAEEND%')
+        s = parseString(s).toprettyxml()
         # remove line breaks
-        s = re.compile('>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL).sub('>\g<1></', s)
+        s = re.compile(r'>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL).sub(r'>\g<1></', s)
         # restore cdata
         s = s.replace('%CDATAESTART%', '<![CDATA[').replace('%CDATAEEND%', ']]>')
         # remove xml header
         s = s.replace("<?xml version=\"1.0\" ?>", "").strip()
-        if xmlheader: 
-                s = xmlheader.group().decode("utf-8") + "\n" + s 
+        if xmlheader:
+            s = xmlheader.group() + "\n" + s
         return s
 
     def check_enabled(self, language):
@@ -113,5 +112,11 @@ class IndentJsonCommand(BaseIndentCommand):
         return ((language == "json") or (language == "plain text"))
 
     def indent(self, s):
-        parsed = json.loads(s)
-        return json.dumps(parsed, sort_keys=True, indent=4, separators=(',', ': '))
+        try:
+            parsed = json.loads(s, object_pairs_hook=OrderedDict, parse_float=decimal.Decimal)
+            indented = json.dumps(parsed, sort_keys=False, indent=4, separators=(',', ': '), use_decimal=True)
+            return indented
+        except Exception:
+            import sys
+            exc = sys.exc_info()[1]
+            sublime.status_message(str(exc))
